303bdc7ee9ae4ea16097d8e4d9cef1738faa2205
reueljonathan <reuel.jonathan@gmail.com>
2014-03-25 01:33:34 -0300

    Puzzle Line
    Arts

--- puzzle_line/js/main.js
+++ puzzle_line/js/main.js
@@ -37,71 +37,105 @@ function counter(){
 
 //Gamvas
 circleBrain = gamvas.ActorState.extend({
+	value: 0,
 	update: function(){
 		if(this.actor.scaleFactor < 1){
 			this.actor.scale(0.1);
 		}
 
 		if(this.actor.position.x > this.actor.newPos.x){
-			this.actor.move(-20,0);
+			this.actor.move(-10,0);
+			//this.actor.rotation = this.actor.position.y > 240 ? Math.atan((240-this.actor.position.x)/(240-this.actor.position.y))*-1 : Math.PI-Math.atan((240-this.actor.position.x)/(240-this.actor.position.y));
 		}
 		if(this.actor.position.x < this.actor.newPos.x){
-			this.actor.move(20,0);
+			this.actor.move(10,0);
+			//this.actor.rotation = this.actor.position.y > 240 ? Math.atan((240-this.actor.position.x)/(240-this.actor.position.y))*-1 : Math.PI-Math.atan((240-this.actor.position.x)/(240-this.actor.position.y));
 		}
 		if(this.actor.position.y > this.actor.newPos.y){
-			this.actor.move(0,-20);
+			this.actor.move(0,-10);
+			//this.actor.rotation = this.actor.position.y > 240 ? Math.atan((240-this.actor.position.x)/(240-this.actor.position.y))*-1 : Math.PI-Math.atan((240-this.actor.position.y)/(240-this.actor.position.x));
 		}
 		if(this.actor.position.y < this.actor.newPos.y){
-			this.actor.move(0,20);
+			this.actor.move(0,10);
+			//this.actor.rotation = this.actor.position.y > 240 ? Math.atan((240-this.actor.position.x)/(240-this.actor.position.y))*-1 : Math.PI-Math.atan((240-this.actor.position.x)/(240-this.actor.position.y));
 		}
-	}
+
+		if (this.actor.position.x > 240) {
+			if (this.actor.position.y < 240) { //1º Quadrante
+				this.actor.rotation = Math.PI + Math.atan((this.actor.position.x-240)/(240-this.actor.position.y));
+			}else{ //4º Quadrante
+				this.actor.rotation = -Math.atan((this.actor.position.x-240)/(this.actor.position.y-240));
+			}
+		}else{
+			if (this.actor.position.y < 240) { //2º Quadrante
+				this.actor.rotation = Math.PI - Math.atan((240-this.actor.position.x)/(240-this.actor.position.y));
+			}else{ //3º Quadrante
+				this.actor.rotation = Math.atan((240-this.actor.position.x)/(this.actor.position.y-240));
+			}
+		}
+
+
+	},
 });
 circle = gamvas.Actor.extend({
 	create: function(name,x,y,file, type){
 		this._super(name,x,y);
-		this.setCenter(40,40);
+		//this.setCenter(30,30);
+		this.setCenter(35,30);
 		this.setScale(0);
 		this.resource = gamvas.state.getCurrentState().resource;
-		this.setFile(file,80,80,1,1);
-		this.rect = {x:this.position.x-40, y:this.position.y-40, w:80, h:80};
+		this.rotation = this.position.y > 240 ? Math.atan((240-this.position.x)/(240-this.position.y))*-1 : Math.PI-Math.atan((240-this.position.x)/(240-this.position.y));	
+		this.type = type;
+		this.setFile(file,70,73,10,1);
+		this.setFrameList([type]);
+		this.rect = {x:this.position.x-30, y:this.position.y-30, w:60, h:60};
 		this.addState(new circleBrain("b"), true);
 		this.newPos = {x: x, y: y};
+		this.resource = gamvas.state.getCurrentState().resource;
+		//this.shadow = new gamvas.Image(this.resource.getImage("./img/shadow.png"));
+		//this.shadow.position = new gamvas.Vector2D(this.position.x + 5, this.position.y + 5);*/
 	}
 });
 
 
 gameplay = gamvas.State.extend({
 	init: function(){
-		this.camera.setPosition(300,300);
+		this.camera.setPosition(240,240);
 		this.clearColor = "rgba(0,0,0,0)";
 		this.table = new Array(new Array(),new Array(),new Array(),new Array(),new Array(),new Array());
 		this.numPieces = 0;
+		this.shadow = new gamvas.Image(this.resource.getImage("./img/shadow.png"));
+		this.back = new gamvas.Image(this.resource.getImage("./img/table.png"));
+		this.pieces = this.resource.getImage("./img/ball.svg");
 		
 		for(var i =0; i<6; i++){
 			for(var j =0; j<6; j++){
-					this.table[i].push({x: i*100, y:j*100, w:100, h:100, c:null, active: false});
+				this.table[i].push({x: i*80, y:j*80, w:80, h:80, c:null, active: false});
 			}			
 		}
 		this.table[0][0] = this.table[0][5] = this.table[5][0] = this.table[5][5] = null;
 
-		this.back = new gamvas.Image(this.resource.getImage("./img/table.png"));
-
-		this.one = this.resource.getImage("./img/1.svg");
-		this.two = this.resource.getImage("./img/2.svg");
-		this.tree = this.resource.getImage("./img/3.svg");
-		this.four = this.resource.getImage("./img/4.svg");
-
 		this.active = new gamvas.Image(this.resource.getImage("./img/active.png"),0,0);
-		
-		this.circle = new circle("c",150,150,this.one,1);
+
+		this.circle = new circle("c",120,120,this.pieces,Math.round(Math.random()*10)%10);
 		this.table[1][1].c = this.circle;
 		this.addActor(this.circle);
 		this.registerInputEvents(this.circle);
 
-		this.circle2 = new circle("c2",150,350,this.two,2);
+		this.circle2 = new circle("c2",120,280,this.pieces,Math.round(Math.random()*10)%10);
 		this.table[1][3].c = this.circle2;
 		this.addActor(this.circle2);
 		this.registerInputEvents(this.circle2);
+
+		this.circle3 = new circle("c3",(3*80)+40,(1*80)+40,this.pieces,Math.round(Math.random()*10)%10);
+		this.table[3][1].c = this.circle3;
+		this.addActor(this.circle3);
+		this.registerInputEvents(this.circle3);
+
+		this.circle4 = new circle("c4",(4*80)+40,(4*80)+40,this.pieces,Math.round(Math.random()*10)%10);
+		this.table[4][4].c = this.circle4;
+		this.addActor(this.circle4);
+		this.registerInputEvents(this.circle4);
 	},
 	preDraw: function(){
 		this.back.draw();
@@ -110,13 +144,16 @@ gameplay = gamvas.State.extend({
 		for(var i = 0; i<6; i++){
 			for(var j = 0; j<6;j++){
 				if ( this.table[i][j]!= null && this.table[i][j].active) {
-					this.active.setPosition(i*100,j*100);
+					this.active.setPosition(i*80,j*80);
 					this.active.draw();
 				}
 			}
 		}
 
 		pointsHud.innerHTML = gamvas.screen.getFPS();
+	},
+	postDraw: function(){
+		this.shadow.draw();
 	},
 	collide: function(a, b){
 		return !(a.x > b.x+b.w ||
@@ -183,8 +220,8 @@ gameplay = gamvas.State.extend({
 								
 						}else if(this.table[i][j].active && 
 							this.collide(this.table[i][j],{x:gamvas.mouse.getX(), y:gamvas.mouse.getY(), w:1, h:1})){
-							this.actualPiece.newPos.x = this.table[i][j].x+50;
-							this.actualPiece.newPos.y = this.table[i][j].y+50;
+							this.actualPiece.newPos.x = this.table[i][j].x+40;
+							this.actualPiece.newPos.y = this.table[i][j].y+40;
 							//this.actualPiece.setPosition(this.table[i][j].x+50,this.table[i][j].y+50);
 							this.table[i][j].c = this.actualPiece;
 							this.actualPiece = null;
