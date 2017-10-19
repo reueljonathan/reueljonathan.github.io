b61b4aa622752e369166db5db421861f8bfe453d
Reuel Jonathan <reuel.jonathan@gmail.com>
2017-10-19 02:36:30 -0300

    Create simple node server for testing, and template pages

--- js/index.js
+++ js/index.js
@@ -8,3 +8,20 @@ xhttp.onreadystatechange = function(){
 
 xhttp.open("GET", "pages/about.html", true);
 xhttp.send();
+
+
+document.querySelectorAll(".nav-link").forEach(function(a){
+	a.onclick = function(evt){
+		evt.preventDefault();
+		var xhttp = new XMLHttpRequest();
+
+		xhttp.onreadystatechange = function(){
+			if(this.readyState == 4 && this.status == 200){
+				document.getElementById("content").innerHTML = xhttp.responseText;
+			}
+		}
+
+		xhttp.open("GET", "pages" + this.getAttribute("href") + ".html", true);
+		xhttp.send();
+	}
+});
